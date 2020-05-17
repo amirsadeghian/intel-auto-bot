68097116eeeeff4186097a39358e9dec069569ff
Amir Sadeghian <amirsa97@gmail.com>
2020-05-16 22:28:28 -0500

    First commit

---
+++ src/intel-auto-bot.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : intel-auto-bot.cpp
+// Author      : Amir Sadeghian
+// Version     :
+// Copyright   : Copyright 2020 by Amir Sadeghian www.sadeghian.us
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
